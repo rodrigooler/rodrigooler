4e4783f7032deb6ed0a314f80cb20f5c87aefde0
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-10-28 01:19:38 -0300

    add new codes

---
+++ src/i18n.ts
@@ -0,0 +1,36 @@
+import i18n from 'i18next'
+import { initReactI18n } from 'react-i18next/dist/commonjs/hooks'
+import Backend from 'i18next-xhr-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+// not like to use this?
+// have a look at the Quick start guide
+// for passing in lng and translations on init
+i18n
+  // load translation using xhr -> see /public/locales
+  // learn more: https://github.com/i18next/i18next-xhr-backend
+  .use(Backend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18n)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    backend: {
+      loadPath: 'static/locales/{{lng}}/{{ns}}.json',
+    },
+    fallbackLng: 'en',
+    debug: false,
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    // special options for react-i18next
+    // learn more: https://react.i18next.com/components/i18next-instance
+    react: {
+      wait: true,
+    },
+  })
+
+export default i18n

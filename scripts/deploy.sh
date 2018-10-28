8b8fc6a1f0f29fe0daed1bff9e015b606b29968f
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-10-28 00:35:31 -0300

    remove files

--- scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-now ls
-now rm oler --yes
-now --public
-now alias
--- src/apollo/client.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-
-export const apolloClient = new ApolloClient({
-    link: new HttpLink({ uri: 'https://api.github.com/graphql' }),
-    cache: new InMemoryCache(),
-})

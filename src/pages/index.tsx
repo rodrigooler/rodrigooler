db79c2d37fc4fd21e35c79aaef48efeeeb7bbd5c
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-10-28 02:55:36 -0300

    create Avatar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,7 @@
-export default () => <h1>DUHASHUDASHUDUHAS</h1>
+import Avatar from '../atoms/Avatar'
+
+export default () => (
+  <>
+    <Avatar bgImg="https://avatars2.githubusercontent.com/u/5496931?s=460&v=4" />
+  </>
+)
